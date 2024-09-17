27d71428ea782e8b73ba772ea54ee2d665171ba4
QuentinGP <quentgeoffpro@yahoo.com>
2024-09-17 16:38:33 +0200

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./assets/styles/global.css";
+import styles from "./assets/styles/main.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1>Test</h1>
         <Image
           className={styles.logo}
           src="https://nextjs.org/icons/next.svg"
