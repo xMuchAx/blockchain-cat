f5c34b5db4f5cb9a6a6200dffd71c1fe21515e04
QuentinGP <quentgeoffpro@yahoo.com>
2024-09-18 09:52:32 +0200

    creation pages

---
+++ src/app/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import "./../assets/styles/global.css";
+import styles from "./../assets/styles/main.module.css";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.page}>
+      <main></main>
+    </div>
+  );
+}
---
+++ src/app/pages/inscription.tsx
@@ -0,0 +1,10 @@
+import "./../assets/styles/global.css";
+import styles from "./../assets/styles/main.module.css";
+
+export default function Inscription() {
+  return (
+    <div className={styles.page}>
+      <main></main>
+    </div>
+  );
+}
