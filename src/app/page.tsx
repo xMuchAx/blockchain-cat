6b90d42d165c8c86695b095fbc714265a958b02d
smoukeuur <enzotang@live.fr>
2024-09-18 16:20:14 +0200

    [Dev] Homepage, Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,52 @@
-import "./assets/styles/global.css";
-import styles from "./assets/styles/main.module.css";
+import "./assets/styles/Global.css";
+import styles from "./assets/styles/Page.module.css";
+import Image from "next/image";
+import Button from "../components/Button/Button";
+import CoinWallet from "./assets/images/svg/wallet/wallet-cat__full.svg";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main>
-        <h1>Titre 1</h1>
-        <h2>Titre 2</h2>
-        <p>Paragraphe</p>
-      </main>
-    </div>
-  );
+    return (
+        <div className={styles.homepageWrapper}>
+            <main>
+                <section className={styles.heroWrapper}>
+                    <div className={styles.heroContent}>
+                        <div className={styles.heroText}>
+                            <h1>
+                                Le <span>chat</span> qui bâtit ton{" "}
+                                <span>trésor</span> une <span>pelle</span> à la
+                                fois
+                            </h1>
+                            <p>
+                                CAT², la monnaie numérique qui allie
+                                transparence, sécurité et décentralisation, pour
+                                bâtir ton trésor en toute confiance.
+                            </p>
+                            <Button variant="primary" rounded={false}>
+                                Commencer l&apos;aventure
+                            </Button>
+                            <div className="eclipse"></div>
+                        </div>
+                        <div className="hero-image">
+                            <Image src={CoinWallet} alt="Coin Wallet"></Image>
+                        </div>
+                    </div>
+                    <div className={styles.heroBottom}>
+                        <div className={styles.heroBar}>
+                            <span>Transparence</span>
+                            <div className={styles.heroBarDot}></div>
+                            <span>Décentralisation</span>
+                            <div className={styles.heroBarDot}></div>
+                            <span>Sécurité</span>
+                        </div>
+                        <div className={styles.heroAnchor}>
+                            <span>Découvrir</span>
+                            <div className={styles.heroArrow}>
+                                <div className={styles.heroDotArrow}></div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./Button.css";
+
+type ButtonProps = {
+    variant: "primary" | "secondary";
+    rounded: boolean;
+    children: React.ReactNode;
+    onClick?: () => void;
+};
+
+const Button: React.FC<ButtonProps> = ({
+    variant,
+    rounded,
+    children,
+    onClick,
+}) => {
+    return (
+        <button
+            onClick={onClick}
+            className={`btn ${
+                variant === "primary" ? "primary" : "secondary"
+            } ${rounded ? "btn-round" : ""}`}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
