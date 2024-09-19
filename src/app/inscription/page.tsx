5fbafd4f46ab44b27bb6b0105f405cea3475b8fe
Antoine Rios Campo <antoine.rios-campo@ens.uvsq.fr>
2024-09-19 15:10:31 +0200

    [Transfer] Signup page

--- src/app/inscription/page.tsx
+++ src/app/inscription/page.tsx
@@ -1,79 +1,73 @@
-import React, {ReactNode} from "react";
+import React, { ReactNode } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-import styles from "./inscription_page.module.css";
+import Image from "next/image";
+import CoinWallet from "../assets/images/svg/wallet/wallet-cat__full.svg";
+import Logo from "../assets/images/svg/logo/logo-cat__white.svg"
+import styles from "./Inscription.module.css";
+import Button from "@/components/Button/Button";
 
-interface FrameProps {
-    children: ReactNode;
-}
-
-// Composant Frame
-const Frame: React.FC<FrameProps> = ({ children }) => {
-    return (
-        <div className={styles.frame}>
-            {children}
-        </div>
-    );
-};
 const Inscription: React.FC = () => {
     return (
         <section className={styles.inscription_page}>
             <div className={styles.left}>
-                <img src="./assets/images/svg/wallet/wallet-cat_full.svg" className={styles.logo} />
-            </div>
+            <Image src={CoinWallet} alt="CoinWallet"></Image>
+           </div>
 
             <div className={styles.right}>
-                <Frame>
-                    <h1>CAT²</h1>
-                    <h2>, le chat qui batit ton trésor</h2>
+                <Image src={Logo} alt="Logo"></Image>
+                <h1><span>CAT²</span>, le chat qui batit ton trésor</h1>
+                <div>
                     <form>
                         <div className={styles.formGroup}>
                             <label htmlFor="Username">Nom d'utilisateur</label>
-                            <input 
-                                type="text" 
-                                id="Username" 
-                                name="Username"
-                            />
+                            <input type="text" placeholder="Entrez votre nom d'utilisateur" id="Username" name="Username" />
                         </div>
 
                         <div className={styles.formGroup}>
-                            <label htmlFor='email'>Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                            />
+                            <label htmlFor="email">Email</label>
+                            <input type="email" placeholder="Entrez votre adresse mail" id="email" name="email" />
                         </div>
 
                         <div className={styles.formGroup}>
                             <label htmlFor="password">Mot de passe</label>
-                            <input 
-                                type="password"
-                                id="password"
-                                name="password"
-                            />
+                            <input type="password" placeholder="Entrez votre mot de passe" id="password" name="password" />
                         </div>
-
-                        <div className={styles.formGroup}>
-                            <label htmlFor="clepublique">Clé publique</label>
-                            <input 
-                                type="text"
-                                id="clepublique"
-                                name="clepublique"
-                            />
+                        <div className={styles.key}>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="clepublique">Clé publique</label>
+                                <input
+                                    type="text" 
+                                    placeholder="Entrez votre clé publique"
+                                    id="clepublique"
+                                    name="clepublique"/>
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label htmlFor="cleprivee">Clé privée</label>
+                                <input
+                                    type="text"
+                                    placeholder="Entrez votre clé privée"
+                                    id="cleprivee"
+                                    name="cleprivee"/>
+                            </div>
+                            
                         </div>
-
-                        <div className={styles.formGroup}>
-                            <label htmlFor="cleprivee">Clé privée</label>
-                            <input 
-                                type="text"
-                                id="cleprivee"
-                                name="cleprivee"
-                            />
+                        <div className={styles.conditions}>
+                           <p> J'accepte les conditions générales d'utilisations...</p>
                         </div>
-
-                        <button type="submit" className={styles.submitButton}>S'inscrire</button>
+                        
+                        <div className={styles.button}>
+                            <Button variant="primary" rounded={false}>
+                            S'inscrire
+                            </Button>
+                        </div>
+                        
                     </form>
-                </Frame>
+                    <div className={styles.link}>
+                    <span>Déjà un compte?</span>
+                    <Link>Connectez-vous</Link>
+                    </div>
+                    
+                </div>
             </div>
         </section>
     );
